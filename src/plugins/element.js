acd708745c20b1aaf7eaf558e1ce55b2a20b5217
niutengfei <niutengfei@magicompass.cn>
2019-12-02 07:52:40 +0800

    element

---
+++ src/plugins/element.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import { Button ,Form,FormItem,Select,Option,Input,DatePicker,Message} from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(Input)
+Vue.use(DatePicker)
+Vue.prototype.$message = Message;
